2819fd661fb4b79aaf2f38333688abc8bc9db359
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-07 20:37:18 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  n = 0
+  new_collection = []
+  while n < collection.length
+    if (yield collection[n]) == true
+      new_collection << collection[n]
+    end
+    n += 1
+  end
+  new_collection
 end
